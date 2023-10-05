ce8dcacd7c1805ba70e66bb0cc80b54e110df778
Albertino Neto <albeneto72@gmail.com>
2023-10-04 21:24:23 -0300

    Iniciando o projeto com Spring Batch

---
+++ src/main/java/br/com/batch/appbatch/AppbatchApplication.java
@@ -0,0 +1,13 @@
+package br.com.batch.appbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppbatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/batch/appbatch/AppbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.batch.appbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
